04768f249eb337730253c4ec2553c791b4a06898
Virginie Dourson <virginiedourson@gmail.com>
2023-09-28 14:59:18 +0200

    End of work

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ firstName.addEventListener('keyup', (e) =>
 let age = inputs[1];
 let hardTruth = document.querySelector('#a-hard-truth');
 age.addEventListener('keyup', (e) => {
-    if ((e.code == "Enter") && (e.target.value < 18)) {
+    if ((e.key == "Enter") && (e.target.value < 18)) {
         hardTruth.style.visibility = "visible";
     }
     else {
@@ -30,7 +30,7 @@ age.addEventListener('keyup', (e) => {
 //Add a keyup listener on both fields and show a visual hint if the password is too short
 // (less than 6 characters) or if the password and its confirmation do not match.
 const getRed = (e)  => {
-    if ((e.key == "Enter") && (e.target.value.length < 6)){
+    if (((e.key == "Enter")||(e.key == "Tab")) && (e.target.value.length < 6)){
         e.target.style.backgroundColor = "red";
     } else {
         e.target.style.backgroundColor = "white";
@@ -39,8 +39,27 @@ const getRed = (e)  => {
 
 let password1 = inputs[2];
 let password2 = inputs[3];
-password1.addEventListener('keyup', getRed);
-password1.addEventListener('keyup', (e) => {
+password1.addEventListener('keydown', getRed);
+password2.addEventListener('keydown', (e) => {
     getRed(e);
-
+    if ((e.key == "Enter")&&(password1.value !== password2.value)){
+        password1.style.backgroundColor = "gold";
+        password2.style.backgroundColor = "gold";
+    } else {
+        password1.style.backgroundColor = "white";
+        password2.style.backgroundColor = "white";
+    }
 });
+
+//Use a change listener on the <select> field to toggle a dark mode on the whole page, 
+//turning the background black and the text white.
+let select = document.getElementById('toggle-darkmode');
+select.addEventListener('change', (e) => {
+    if (e.target.value == "dark") {
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white"
+    } else {
+        document.body.style.backgroundColor = "white";
+        document.body.style.color = "black";
+    } 
+})
